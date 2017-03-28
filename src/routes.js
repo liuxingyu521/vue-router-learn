d4028e0b6914de656817a0d0f483c14d31aa15ed
jian.xu1 <jian.xu1@corp.elong.com>
2017-03-28 11:13:37 +0800

    modify the route config

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 export default [
     { path: '/', component: require('./pages/login.vue'), redirect: '/login' },
     {
-      path: '/user/:id',
-      component: require('./pages/userMain.vue'),
+      path: '/user/:id', component: require('./pages/userMain.vue'),
       children: [
-          { path: 'flow', name: 'flow', component: require('./pages/subPage/flow.vue') },
-          { path: 'statistics', name: 'statistics', component: require('./pages/subPage/statistics.vue'), redirect: 'pieChart' },
-          { path: 'keepAccount', name: 'keepAccount', component: require('./pages/subPage/keepAccount.vue')}
+          { path: 'flow', name: 'flow', component: require('./pages/subPage/flow.vue')},
+          { path: 'statistics', name: 'statistics', component: require('./pages/subPage/statistics.vue')},
+          { path: 'keepAccount', name: 'keepAccount', component: require('./pages/subPage/keepAccount.vue')},
+          { path: 'homepage', name: 'homepage', component: require('./pages/subPage/homepage.vue')},
+          { path: 'editProfile', name: 'editProfile', component: require('./pages/subPage/editProfile.vue')}
       ]
-    }
+    },
+    { path: '/login', component: require('./pages/login.vue')},
+    { path: '/register', component: require('./pages/register.vue')}
+]
