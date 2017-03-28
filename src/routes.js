311cf5f837b4bff763540153dcc45137eaf84463
jian.xu1 <jian.xu1@corp.elong.com>
2017-03-28 10:37:52 +0800

    add keepAccount path

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 export default [
     { path: '/', component: require('./pages/login.vue'), redirect: '/login' },
-    { path: '/user/:id', component: require('./pages/userMain.vue'),
+    {
+      path: '/user/:id',
+      component: require('./pages/userMain.vue'),
       children: [
-        { path: 'flow', name: 'flow', component: require('./pages/subPage/flow.vue')},
-        { path: 'statistics', name: 'statistics', component: require('./pages/subPage/statistics.vue'), redirect: 'pieChart'}
+          { path: 'flow', name: 'flow', component: require('./pages/subPage/flow.vue') },
+          { path: 'statistics', name: 'statistics', component: require('./pages/subPage/statistics.vue'), redirect: 'pieChart' },
+          { path: 'keepAccount', name: 'keepAccount', component: require('./pages/subPage/keepAccount.vue')}
       ]
     }
-]
