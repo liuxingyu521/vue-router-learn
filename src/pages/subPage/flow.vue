4b218c9b06c0458e7b62cdf540c71a27061566f8
liuxingyu521 <xuj_power@qq.com>
2017-04-11 17:11:58 +0800

    feat(流水页): 增加日历显示

--- src/pages/subPage/flow.vue
+++ src/pages/subPage/flow.vue
@@ -4,11 +4,16 @@
       <div class="calendar">
         <span class="static">today</span>
         <span class="c-month">{{ date.c_month }}月</span>
-        <span class="c-day">{{ date.c_day }}</span>
+        <span class="c-day"><b>{{ date.c_day }}</b></span>
       </div>
       <div class="display">
-        <span>年流水</span>
-        <span>2017年</span>
+        <div class="display-title">
+          <select class="flow-type" v-model="flowType">
+            <option value="year" selected="selected">年流水</option>
+            <option value="month">月流水</option>
+          </select>
+          <div class="flow-date">{{ flowType }}</div>
+        </div>
       </div>
     </div>
   </div>
@@ -29,8 +34,9 @@
         date: {
           c_year: '2017',
           c_month: '4',
-          c_day: '10'
-        }
+          c_day: '06'
+        },
+        flowType: 'year'
       }
     }
     // created: function(){
@@ -60,12 +66,21 @@
     margin-top: 3.5rem;
     position: relative;
   }
+  .container .totalDisplay{
+    height: 12rem;
+    border-bottom: 1px solid blue;
+    background-color: #fbf0d3;
+  }
   .container .calendar{
     position: absolute;
-    top: .5rem;
-    left: 1rem;
-    width: 30%;
-    border: 1px solid #aaa;
+    top: .7rem;
+    left: .6rem;
+    width: 33%;
+    padding: 3px;
+    border: 2px solid #ccc;
+    border-radius: 2px;
+    box-shadow: 1px 2px 1px 1px rgba(49, 45, 45, 0.52);
+    background-color: white;
   }
   .container .display{
     padding-left: 40%;
@@ -76,19 +91,52 @@
   }
   .container .calendar span.static{
     font-family: "comic sans MS";
-    font-size: 2rem;
+    font-size: 2.5rem;
     text-align: center;
-    height: 2.6rem;
+    height: 2.8rem;
+    color: #fd5757;
   }
   .container .calendar span.c-month{
-    font-size: 1.7rem;
-    text-indent: 0.8rem;
-    height: 2.8rem;
+    font-size: 1.9rem;
+    text-indent: 1rem;
+    height: 3.2rem;
   }
   .container .calendar span.c-day{
-    font-size: 3rem;
-    text-indent: 1.6rem;
+    font-size: 3.5rem;
+    text-indent: 2rem;
     height: 4rem;
-    text-decoration: underline;
+  }
+  .container .calendar span.c-day b:after{
+    content: '';
+    position: absolute;
+    bottom: 0.5rem;
+    left: 2.5rem;
+    width: 3.5rem;
+    border-bottom: 2px solid #505050;
+  }
+  .container .display .display-title{
+    height: 2.5rem;
+    padding: .8rem .3rem;
+  }
+  .container .display .display-title [class^='flow']{
+    display: inline-block;
+    height: 2.5rem;
+    line-height: 2.5rem;
+  }
+  .container .display .display-title select.flow-type{
+    margin: 0 .5rem 0 1rem;
+    padding: 0 1rem 0 .5rem;
+    box-shadow: inset -2px -3px 2px 0px rgba(204, 204, 204, .8);
+    border-radius: .2rem;
+    background-color: #eee;
+    background: url('../../assets/img/arrow.png') no-repeat right center #eee;
+    font-size: 1rem;
+    /* 去除下拉框小三角 */
+    appearance:none;
+    -moz-appearance:none;
+    -webkit-appearance:none;
+  }
+  .container .display .display-title select.flow-type:focus{
+    outline: none;
   }
 </style>
