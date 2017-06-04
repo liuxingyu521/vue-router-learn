0046767d4f8c93d67891a3086efa03b83c291ad5
Richard <xuj_power@qq.com>
2017-06-04 22:44:58 +0800

    add: 404Page

---
+++ src/pages/404.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="notfound">
+		<span @click="goBack"></span>
+	</div>
+</template>
+
+<script>
+	export default {
+		methods: {
+			goBack: function(){
+				this.$router.push('flow');
+			}
+		}
+	}
+</script>
+
+<style>
+	.notfound{
+		background-image: url('../assets/img/404.png');
+		background-size: contain;
+	}
+	.notfound span{
+    display: block;
+    width: 9.7rem;
+    height: 2.2rem;
+    position: absolute;
+    top: 23.8rem;
+    left: 6.9rem;
+    border-radius: 3rem;
+	}
+</style>
